990c241bee61e432f3633c1f00eef1791962c930
RodrigoMarques23 <rodrigomarques6923@gmail.com>
2019-05-23 00:22:09 +0100

    work

---
+++ Assets/Scripts/Tp_V.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tp_V : MonoBehaviour
+{
+    
+        public Transform teleportDestination;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        void OnTriggerEnter2D(Collider2D d)// é 'c' mas pode ser qlqr coisa 
+        {
+            if (d.gameObject.tag == "Teleport_V")
+            {
+                transform.position = teleportDestination.position;
+                Debug.Log("colider");
+            }
+        }
+
+}
+
